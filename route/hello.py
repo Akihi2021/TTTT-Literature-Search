49389ded616b12bf2c999bfbafa94b5e4d5d9eed
jinxuanyu <jinxuanyu@xiaoice.com>
2022-12-02 14:33:26 +0900

    [chore] add template for POST methods

--- route/hello.py
+++ route/hello.py
@@ -1,14 +1,17 @@
-from flask import request, Blueprint
-from flask_restx import fields, Api, Resource
+##################################################
+# Demo route for POST AND GET METHODS
+##################################################
 
+from flask import request, Blueprint
 from context import swagger
 from rest import request_handle, Response, BaseResource
 
-##################################################
-# Demo route
-##################################################
 hello_ns = swagger.namespace('hello', description='APIs for simple testing')
 swagger.add_namespace(hello_ns)
+
+parser = swagger.parser()
+parser.add_argument('a', location='json', type=int, required=True, help="operand a")
+parser.add_argument('b', location='json', type=int, required=True, help='operand b')
 
 
 @hello_ns.route('/test-add')
@@ -26,4 +29,13 @@ class TestAdd(BaseResource):
         resp.data['a'] = a
         resp.data['b'] = b
         resp.data['sum'] = sm
+        return resp
+
+    @hello_ns.doc('test a + b with post')
+    @hello_ns.expect(parser)
+    @request_handle
+    def post(self):
+        args = parser.parse_args()
+        resp = Response(data={})
+        resp.data["ans"] = args["a"] + args["b"]
         return resp
