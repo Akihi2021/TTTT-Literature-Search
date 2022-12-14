0f8b7bf66bb2f54b22648f8dd8c010da80e2f7a3
jinxuanyu <jinxuanyu@xiaoice.com>
2022-12-14 13:55:19 +0900

    [feat] limit related authors num

--- service/author.py
+++ service/author.py
@@ -5,9 +5,12 @@ def get_papers(author_id):
     works = []
 
     try:
-        works = openAlex.get_list_of_works(filters={
+        works = openAlex.get_list_of_works(
+            filters={
             'author.id': 'https://openalex.org/A' + author_id
-        })
+            },
+            per_page=200
+        )
     finally:
         return works
 
@@ -24,6 +27,9 @@ def get_id_and_title_and_type_of_papers(author_id):
                     'title': result.get('title'),
                     'type': result.get('type')
                 })
+
+                if len(data) > 200:
+                    return data
     finally:
         return data
 
@@ -64,4 +70,5 @@ def get_related_authors(author_id):
 
 
 if __name__ == "__main__":
-    print(len(get_related_authors("2175547780")))
+    # print(len(get_related_authors("2175547780")))
+    print(len([_ for _ in get_id_and_title_and_type_of_papers("2175547780")]))
